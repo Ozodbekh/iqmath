b9efee59f118b15e7e84dc5e0a6b90dffb0306a3
ozodbekh <ozodbekg800@gmail.com>
2025-02-18 12:15:50 +0500

    Django project first commit

---
+++ iq_math/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+ name='iq_math',
+ version='0.1',
+ description='A light weight anti crawler app for Django.',
+ url='https://github.com/Ozodbekh/iqmath.git',
+ author='Ozodbek',
+ author_email='ozodbekg800@gmail.com',
+ license='MIT',
+ packages=['iq_math'],
+ zip_safe=False
+)
